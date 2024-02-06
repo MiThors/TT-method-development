2a1af4ed4c9dfe47b218d6e2cb1fb8e66931687a
Milo Thordarson <anth2886@student.uu.se>
2024-02-06 14:14:44 +0100

    Func and args began

--- Pipeline/TT_Method.py
+++ Pipeline/TT_Method.py
@@ -9,9 +9,16 @@ Milo Thordarson: anth2886@student.uu.se'''
 import sys
 import gzip
 import TT_functions
+import argparse
 from zipfile import ZipFile
 
-# Arguments from commandline
+# Arguments from commandline using argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--in",  nargs=2, help="2 genotype files for populations to compare", required=True)
+parser.add_argument("-t", "--type", choices=['vcf', 'tped', 'bam'], help="type of genotype files input", required=True)
+parser.add_argument("-a", "--ancestral", help="file containing ancestral states", required=True)
+parser.add_argument("-k", "--keywords",  nargs=2, help="names of 2 populations, MUST be same order as in --in")
+args = parser.parse_args()
 
 # Filtering parameters that can be changed by the user
 
