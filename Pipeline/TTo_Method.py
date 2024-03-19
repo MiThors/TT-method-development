f254b3c02c19c6c89e0250913678e07e5a913f4c
Milo Thordarson <anth2886@student.uu.se>
2024-03-19 14:57:24 +0100

    Parallelization, windows, print counts

--- Pipeline/TTo_Method.py
+++ Pipeline/TTo_Method.py
@@ -9,6 +9,8 @@ Milo Thordarson: anth2886@student.uu.se'''
 import functions
 import argparse
 import os
+import multiprocessing
+import sys
 
 # Filtering parameters that can be changed by the user
 # These represent what you consider low and high coverage for a genotype position, as an int
@@ -48,6 +50,12 @@ parser.add_argument("-o", "--out",
                     default = "TTo_out_pop1_pop2",
                     help = "name of the output directory")
 parser.add_argument("--test", action = "store_true")
+parser.add_argument("-c", "--counts",
+                    action="store_true",
+                    help = "output a file with all counts per chromosome per window")
+parser.add_argument("-w", "--window",
+                    default = "5000000",
+                    help = "set the window size for calculating local parameters")
 args = parser.parse_args()
 
 # Turn args into informatively named variables
@@ -56,8 +64,12 @@ files_pop2 = args.pop2
 files_outgroup = args.outgroup
 file_type = args.type
 files_anc = args.ancestral
-pop1_key, pop2_key = args.keywords
+pop1_key = args.keywords[0] 
+pop2_key = args.keywords[1]
+outg_key = args.keywords[2]
 out_dir = args.out
+print_counts = args.counts
+win_size = args.window
 
 # Check that filepaths are the same lengths
 file_tot = len(files_pop1)
@@ -67,7 +79,7 @@ if any(len(lst) != file_tot for lst in [files_pop2, files_outgroup, files_anc]):
     print(f'Files given for pop2 are: {files_pop2}')
     print(f'Files given for outgroup are: {files_outgroup}')
     print(f'Files given for ancestral states are: {files_anc}')
-    exit(1)
+    sys.exit(1)
 
 # Make output dir, will complain if it already exists, which is why this is so early in the script
 if not args.test: os.mkdir(out_dir)
@@ -77,12 +89,30 @@ counts_dict = {}
 
 # For vcf filetype
 if file_type == 'vcf': 
-    for i in range(file_tot):
-        # Get the counts for a vcf file from this function, returns a dictionary with {chrom: m0, m1, m2, m3, m4, m5, m6, m7, m8} structure
-        add_dict = functions.get_counts_vcf_TTo(files_pop1[i], files_pop2[i], files_outgroup[i], files_anc[i], low_coverage, high_coverage, vcf_filters)
-        for key in add_dict:
-            if key in counts_dict:
-                counts_dict[key] = [counts_dict[key][i] + add_dict[key][i] for i in range(9)]
-            else: counts_dict.update({key: add_dict[key]})
+    # Create iterable list with all input parameters for counting
+    iterables = [[files_pop1[i], files_pop2[i], files_outgroup[i], files_anc[i], low_coverage, high_coverage, vcf_filters, win_size] for i in range(file_tot)]
 
-print(counts_dict)
+# To avoid infinite recursion
+    if __name__ == '__main__':
+        with multiprocessing.Pool() as pool:
+            # Computes for files in parallel using CPU cores available to user
+            results = pool.map(functions.get_counts_vcf_TTo, iterables)
+    
+counts = {}
+windows = {}
+
+for comparison in results:
+    for key in comparison[0]:
+        if not key in counts:
+            counts.update({key: []})
+            windows.update({key: []})
+    counts[key].extend(comparison[0][key])
+    windows[key].extend(comparison[1][key])
+
+if print_counts:
+    with open(out_dir + "/" + pop1_key + pop2_key + "_TTo_Counts.txt", 'w') as count_file:
+        for key in counts:
+            count_file.write("#" + key + "\n")
+            for i in range(len(counts[key])):
+                count_file.write(str(windows[key][i]) + "\t" + str(counts[key][i]) + "\n")
+
